fc3f1ceaf42fb21a5cc4b2f41c41a93a669e71d2
lex-c <chlochlo.lexa@gmail.com>
2021-06-05 04:04:22 -0400

    import in init

--- cc_reproject/__init__.py
+++ cc_reproject/__init__.py
@@ -2,3 +2,8 @@ try:
     from .version import __version__
 except ImportError:
     __version__ = "unknown"
+
+try:
+    from .cc_reproject import cc_reproject
+except ImportError as e:
+    raise ImportError(f"{e}:\n can't find module")
