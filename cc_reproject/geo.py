e74216ea7812e8d73a32cf69dff1cf4c8dbc3ea6
lex-c <chlochlo.lexa@gmail.com>
2021-06-17 17:45:51 -0400

    rewrite for resample transform with default calcs and geo accessor

---
+++ cc_reproject/geo.py
@@ -0,0 +1,12 @@
+import xarray
+from .warp import reproject
+
+
+@xarray.register_dataarray_accessor('geo')
+class CCRasterArray:
+    
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+    
+    def reproject(self, dst_crs, **kwargs):
+        return reproject(self._obj, dst_crs, **kwargs)
