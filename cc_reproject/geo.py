dd24e45367cb374da0fba068ed8f1997a242d99e
lex-c <chlochlo.lexa@gmail.com>
2021-06-19 04:51:50 -0400

    edit geo

--- cc_reproject/geo.py
+++ cc_reproject/geo.py
@@ -8,5 +8,9 @@ class CCRasterArray:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
-    def reproject(self, dst_crs, dst_shape=None, chunks=None, numblocks=None, **kwargs):
-        return reproject(self._obj, dst_crs, dst_shape=dst_shape, chunks=chunks, numblocks=numblocks, **kwargs)
+    def reproject(self, dst_crs, dst_shape=None,
+                  chunks=None, numblocks=None, **kwargs):
+        return reproject(self._obj, dst_crs,
+                         dst_shape=dst_shape, chunks=chunks,
+                         numblocks=numblocks, band_kwargs=None,
+                         **kwargs)
